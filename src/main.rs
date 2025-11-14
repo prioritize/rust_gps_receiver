23132b2e0a0b44a937517c837f4fad6fbd27f2b8
prioritize <jared@prototypic.al>
2025-11-13 16:36:46 -0800

    progress

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ pub mod config;
 use anyhow::Result;
 use anyhow::anyhow;
 use config::{ConfigFile, PortConfig};
-use serialport::TTYPort;
-use std::fs::File;
 use std::time::Duration;
+use tokio::fs::File;
 use tokio::io::AsyncReadExt;
+use tokio::time::Instant;
+use tokio::time::sleep;
+use tokio_serial::SerialPortBuilderExt;
+use tokio_serial::SerialStream;
 use tokio_serial::{DataBits, Parity, StopBits};
 
 // Function that opens the serial port, performs a test read and then returns a result
-pub async fn open_serial_port(config: PortConfig) -> Result<TTYPort> {
+pub async fn open_serial_port(config: PortConfig) -> Result<SerialStream> {
     match tokio_serial::available_ports() {
         Ok(ports) => {
             for p in ports {
@@ -26,7 +29,7 @@ pub async fn open_serial_port(config: PortConfig) -> Result<TTYPort> {
                                 .parity(Parity::None)
                                 .stop_bits(StopBits::One)
                                 .timeout(Duration::from_millis(200))
-                                .open_native()
+                                .open_native_async()
                             {
                                 Ok(port) => return Ok(port),
                                 Err(_) => return Err(anyhow!("Failure to open!")),
@@ -49,22 +52,34 @@ pub async fn open_serial_port(config: PortConfig) -> Result<TTYPort> {
 }
 // Function that consumes all the incoming serial port data and sends it to the appropriate
 // channels
-pub async fn consume_serial_port(port: &mut TTYPort) {
+pub async fn consume_serial_port(port: &mut SerialStream, dur: Option<Duration>) {
     let mut internal_buf = [0u8; 256];
-    loop {
-        match port.read(&mut internal_buf) {
-            Ok(a) => todo!(),
-            Err(_) => todo!(),
+    match dur {
+        Some(d) => {
+            let start = Instant::now();
+            while start.elapsed() < d {
+                match port.read(&mut internal_buf).await {
+                    Ok(_) => println!("{:?}", internal_buf),
+                    Err(_) => todo!(),
+                }
+                sleep(Duration::from_millis(100)).await;
+            }
         }
+        None => loop {
+            match port.read(&mut internal_buf).await {
+                Ok(_) => println!("{:?}", internal_buf),
+                Err(_) => todo!(),
+            }
+        },
     }
 }
 
 #[tokio::main]
 async fn main() {
     // TODO: Open the config file and read the port information
-    let mut file = File::open("./src/gps_config.toml").unwrap();
+    let mut file = File::open("./src/gps_config.toml").await.unwrap();
     let mut contents = String::new();
-    let _ = file.read_to_string(&mut contents).unwrap();
+    let _ = file.read_to_string(&mut contents).await.unwrap();
     let conf: ConfigFile = toml::from_str(&contents).unwrap();
     println!("{conf :?}");
 
@@ -72,10 +87,13 @@ async fn main() {
 
     // TODO: Spawn a thread waits for incoming messages and parses them
     let mut port = tokio_serial::new(conf.port_config.port, conf.port_config.speed)
-        .open_native()
+        .open_native_async()
         .expect("failed to open port");
     let mut serial_buf: Vec<u8> = vec![0; 5];
-    port.read(serial_buf.as_mut_slice()).expect("Found no data");
+    let _ = port
+        .read(serial_buf.as_mut_slice())
+        .await
+        .expect("Found no data");
     println!("{}", String::from_utf8(serial_buf).unwrap());
 }
 
@@ -95,18 +113,7 @@ mod tests {
         let mut port = open_serial_port(config).await?;
         let mut incoming_buffer: Vec<u8> = Vec::new();
         let mut buf = [0u8; 64];
-        for _ in 0..100 {
-            let _ = port.read(buf.as_mut_slice());
-            let cap_string = match String::from_utf8(buf.to_vec()) {
-                Ok(v) => Some(v),
-                Err(_) => {
-                    println!("{buf:?}");
-                    None
-                }
-            };
-            sleep(Duration::from_millis(100));
-            println!("The buffer contains -- {cap_string:?}",);
-        }
+        consume_serial_port(&mut port, Some(Duration::from_secs(2))).await;
         Ok(())
     }
 }
